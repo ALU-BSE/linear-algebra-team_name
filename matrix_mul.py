e67cbd89d44189082c1e12e40de054d3eecd2289
Tamanda Kaunda <136020130+tamandakaunda-15@users.noreply.github.com>
2025-01-20 22:57:38 +0200

    Update matrix_mul.py

--- matrix_mul.py
+++ matrix_mul.py
@@ -17,7 +17,8 @@ Ans = []
 for i in range(len(Prices)):
     row_sum = 0
     for j in range(len(Prices[0])):
-        # COMPLETE THE MISSING LOGIC HERE
+         row_sum += Prices[i][j] * Array2[j]
+              Ans.append(row_sum)
         pass
 
 
