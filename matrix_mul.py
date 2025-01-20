87d2776b3a1e598e9f5db86c47a9faf5894164ca
shemankubana <shemaivan27@gmail.com>
2025-01-20 23:17:48 +0200

    fixed the code

--- matrix_mul.py
+++ matrix_mul.py
@@ -1,25 +1,14 @@
-import pandas as pd
-import numpy as np
-
-# Load data
-data = pd.read_csv('path/to/data')
-
-# Example arrays
 Prices = [[300, 500],
           [1000, 120.85]]
 
 Array2 = [200, 100]
 
-# Calculate the result
 Ans = []
-# (300*200 + 500*100) as an example calculation
 
 for i in range(len(Prices)):
     row_sum = 0
     for j in range(len(Prices[0])):
          row_sum += Prices[i][j] * Array2[j]
-              Ans.append(row_sum)
-        pass
-
+    Ans.append(row_sum)
 
 print("Results calculated with contributions from Tamanda:",Ans)
